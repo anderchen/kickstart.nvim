36bf9fefb89a370f840cfdbe98cdf8fdc6d8280f
Anderson Chen <ander.chen00@gmail.com>
2025-11-18 17:01:17 -0500

    change how custom plugins are initiated

--- init.lua
+++ init.lua
@@ -1023,8 +1023,8 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
-
+  -- { import = 'custom.plugins' },
+  require 'custom.plugins.oil',
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
