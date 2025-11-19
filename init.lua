03586c0623cf5df04f5160581a016f30e8829e42
Anderson Chen <ander.chen00@gmail.com>
2025-11-19 12:56:24 -0500

    configure tab identation

--- init.lua
+++ init.lua
@@ -166,6 +166,10 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Tab identation
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
